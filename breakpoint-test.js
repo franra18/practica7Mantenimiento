4103738b167fdc0f5f89bf877ebb96168c2080e4
projectGeorge <jorgers4@uma.es>
2025-05-22 12:26:11 +0200

    breakpoint-test

---
+++ breakpoint-test.js
@@ -0,0 +1,33 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    /*
+    scenarios: {
+        breakpoint: {
+            executor: 'ramping-arrival-rate', // Incrementa la carga exponencial
+            preAllocatedVUs: 1000, //VUs alocados inicialmente
+            maxVUs: 1e7, //VUs maximo
+            stages: [
+                { duration: '10m', target: 100000 },
+            ]
+        }
+    },
+    */
+    stages: [
+        { duration: '10m', target: 100000 }
+    ],
+    thresholds: {
+        http_req_failed: [{
+        threshold: 'rate<=0.01',
+        abortOnFail: true,
+    }]}
+};
+
+export default () => {
+    const urlRes = http.get('http://localhost:8080/medico/1');
+    check(urlRes, {
+        'status is 200': (r) => r.status === 200,
+    })
+    sleep(1);
+};
